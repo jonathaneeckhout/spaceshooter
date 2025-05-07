0ec21fb0d61935de3a6e80aa569e37323f3c6df8
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2025-05-07 20:20:25 +0200

    run game

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
+#include <csignal>
 
-#include <jengine/JEngine.hpp>
+#include "jengine/Game.hpp"
+
+
+Game *game = NULL;
+
+static void signalHandler(int signum)
+{
+    std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+
+    game->stop();
+}
+
+static void loadResources()
+{
+    // Resources *resources = Resources::getInstance();
+
+}
+
+static void populateGame()
+{
+}
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    game = Game::getInstance();
 
-    JEngine *engine = new JEngine();
+    std::signal(SIGINT, signalHandler);
 
-    delete engine;
+    loadResources();
+
+    populateGame();
+
+    Renderer *renderer = Renderer::getInstance();
+
+    SDL_SetWindowTitle(renderer->window, "Spaceshooter");
+
+    game->run();
+
+    Game::deleteInstance();
 
     return 0;
 }
